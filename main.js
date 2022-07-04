4fa8c62d21244661599cbaf114694c36e22529c9
coderlore <coderlore@gmail.com>
2022-07-03 17:16:07 -1000

    Added styling and fixed li results

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#title').autocomplete({
         source: async function(request, response) {
             let data = await fetch(`http://localhost:8000/search?query=${request.term}`)
-                .then(result => results.json())
+                .then(results => results.json())
                 .then(results => results.map(result => {
                     return {
                         label: result.title,
@@ -14,14 +14,13 @@ $(document).ready(function () {
         },
         minLength: 2,
         select: function(event, ui) {
-            console.log(ui.item.id)
             fetch(`http://localhost:8000/get/${ui.item.id}`)
                 .then(result => result.json())
                 .then(result => {
                     $(`#cast`).empty()
                     result.cast.forEach(cast => 
                         {
-                            $(cast).append(`<li>$(cast)</li>`)
+                            $('#cast').append(`<li>${cast}</li>`)
                         })
                         $(`img`).attr(`src`,result.poster)
                 })
